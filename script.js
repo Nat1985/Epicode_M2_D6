a2f8821822029eb9f2a7d7d2972b5fcac98fac2d
Nat1985 <123695875+Nat1985@users.noreply.github.com>
2023-04-11 19:34:16 +0200

    11 aprile

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-function h1Changer(newText) {
+
+// ESERCIZIO 7:
+
+/* function h1Changer(newText) {
 let h1Element = document.getElementsByTagName("h1");
 h1Element = h1Element[0].innerText = newText;
 console.log(h1Element);
 }
 
-h1Changer("Nuova stringa");
+h1Changer("Nuova stringa"); */
 
+//ESERCIZIO 8:
+
+/* function changeBackground(newColor) {
+    let bodyTook = document.getElementsByTagName("body");
+    console.log(bodyTook);
+    bodyTook[0].style.background = newColor;
+}
+
+changeBackground("grey"); */
+
+// ESERCIZIO 9:
+
+function changeFooter(newUrl) {
+    let footerTook = document.getElementsByTagName("footer.span");
+    console.log(footerTook);
+}
+
+changeFooter("Nuovo indirizzo");
